6cd5810a0e588dcc794b5756d3040e1fbc41a591
MubarekHussen <mubahussen2014@gmail.com>
2022-12-23 11:41:15 +0300

    trying

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,3 +1,5 @@
+/* eslint no-underscore-dangle: ["error", {"allow": ["_maxStudentsSize"] }] */
+
 export default class classRoom {
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
